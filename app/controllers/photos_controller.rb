3609e9b901c62f77b0d2d4ffa5ab7505edd2a7fb
Adam <alk5198@gmail.com>
2016-10-29 14:01:30 -0500

    update
    update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,47 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo = Photo.find_by({ :id=>params[:id]})
+
+
+end
+
+def new_form
+end
+
+def create_row
+  p = Photo.new
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+# render("create_row.html.erb")
+  redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+  i=Photo.find_by({ :id=>params[:id]})
+  i.destroy
+  redirect_to("http://localhost:3000/photos")
+
+end
+
+def edit_form
+  @photo = Photo.find_by({ :id=>params[:id]})
+
+
+end
+def update_row
+    i=Photo.find_by({ :id=>params[:id]})
+    i.caption = params[:the_caption]
+    i.source = params[:the_source]
+    i.save
+  @photo = Photo.find_by({ :id=>params[:id]})
+redirect_to("http://localhost:3000/photos/#{@photo.id}")
+
+
+
+end
 
 end
